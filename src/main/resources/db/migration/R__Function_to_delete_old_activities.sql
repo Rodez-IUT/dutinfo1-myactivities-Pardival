8261648422cb9fbdbbaee4dc70f468de175e5410
Kevin Dufour <kevin.dufour@iut-rodez.fr>
2019-02-13 15:42:36 +0100

    GIT Fix 1.2 function to delete old activities OK

--- src/main/resources/db/migration/R__Function_to_delete_old_activities.sql
+++ src/main/resources/db/migration/R__Function_to_delete_old_activities.sql
@@ -1,3 +1,4 @@
 CREATE OR REPLACE FUNCTION delete_activities_older_than(old_date date) RETURNS int AS $$
+   DELETE FROM activity WHERE  modification_date < '2019-01-10';
    SELECT 1;
 $$ LANGUAGE SQL; 
